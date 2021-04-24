68c7e3fd76b9265b84008b92661bc50105f7a386
eug-vs <eug-vs@keemail.me>
2021-04-25 02:47:51 +0300

    feat: change player to ncspot

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "playerctl metadata --format='{{ artist }} - {{ title }}' --player=spotify 2> /dev/null", 3, 1},
+	{" ", "playerctl metadata --format='{{ artist }} - {{ title }}' --player=ncspot 2> /dev/null", 3, 1},
 	{"", "xkb-switch", 0, 4},
 	{"", "~/.local/share/i3blocks-modules/volume -np", 0, 2},
 	{"", "~/.local/share/i3blocks-modules/backlight -np", 0, 3},
