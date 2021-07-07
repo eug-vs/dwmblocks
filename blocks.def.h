13942ecafb34e16997940716dcbd4e81e6e0d6cb
eug-vs <eug-vs@keemail.me>
2021-07-07 05:47:23 +0300

    feat: add taskwarrior blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "playerctl metadata --format='{{ artist }} - {{ title }}' --player=ncspot 2> /dev/null", 3, 1},
+	{" ", "playerctl metadata --format='{{ artist }} - {{ title }}' --player=spotify 2> /dev/null", 3, 1},
+	{" ", "task _get rc.context", 60, 0},
+	{" ", "task scriptable rc.verbose: +ACTIVE | head -n1", 15, 0},
 	{"", "xkb-switch", 0, 4},
 	{"", "~/.local/share/i3blocks-modules/volume -np", 0, 2},
 	{"", "~/.local/share/i3blocks-modules/backlight -np", 0, 3},
