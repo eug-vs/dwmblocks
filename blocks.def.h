1f8272362e00fecd447eeece4615a1cf9ef1dde9
eug-vs <eugene@eug-vs.xyz>
2022-04-18 19:06:05 +0300

    feat: use xkblayout-state

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" ", "mpc current", 3, 1},
 	{" ", "task _get rc.context", 60, 0},
-	{" ", "task scriptable rc.verbose: +ACTIVE | head -n1", 15, 0},
-	{"", "xkb-switch", 0, 4},
-	{"", "~/.local/share/i3blocks-modules/volume -np", 0, 2},
-	{"", "~/.local/share/i3blocks-modules/backlight -np", 0, 3},
-	{" ", "~/.local/share/i3blocks-modules/wifi -np -f '{ssid}'", 15, 0},
+	{"", "xkblayout-state print %s", 0, 4},
+	{"", "[ $(pulsemixer --get-mute) = 1 ] && echo 🔇 && exit || echo  $(pulsemixer --get-volume | cut -f 1 -d ' ')%", 0, 2},
+	{" ", "wpa_cli status | grep ^ssid= | sed 's/ssid=//'", 15, 0},
 	{"  ", "echo $(cat /sys/class/power_supply/BAT0/capacity)%", 30, 0},
 	{" ", "date '+%a, %d.%m.%Y  %H:%M:%S'", 1, 0},
 };
